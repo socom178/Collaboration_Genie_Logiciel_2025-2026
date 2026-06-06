10bc1331725195ad30f204b6fb0f26df2dc0bad6
unknown <biotagaaudias6@gmail.com>
2026-06-06 16:00:59 +0200

    final

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-    header('Location: /Collaboration_GL_2025-2026/projet_memoires_uatm_complet/projet/presentation/auth/login.php');
+    header('Location: /Collaboration_GL_2025-2026/presentation/auth/login.php');
     exit();
 ?>
